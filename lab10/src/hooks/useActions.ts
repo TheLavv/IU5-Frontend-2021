41296a118c4824971b507a46b63b21b1ab4e12b3
Andrey Golovatskiy <andrey.golovaczkiy@mail.ru>
2021-05-04 19:40:13 +0300

    Revert "lab10 done"
    This reverts commit b050846664a7b7f941661d10c71062fd41f8ba5f.

--- lab10/src/hooks/useActions.ts
+++
@@ -1,8 +0,0 @@
-import {useDispatch} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as userActionCreators from '../actions/user'
-
-export const useActions = () => {
-    const dispatch = useDispatch();
-    return bindActionCreators(userActionCreators, dispatch);
-}
--- lab10/src/utils.ts
+++
@@ -1,5 +0,0 @@
-export default function normalizeUrl(url: string): string {
-    if ((url.split(''))[url.length - 1] === '/')
-        return (url.slice(0, -1));
-    return (url);
-}
